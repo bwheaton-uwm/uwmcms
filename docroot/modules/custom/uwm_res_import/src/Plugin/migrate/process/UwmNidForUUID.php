f2376204a5552abc58dcaeb6307edc06a6554a65
sodacrackers <1248488+sodacrackers@users.noreply.github.com>
2019-01-07 10:49:51 -0800

    UWM-CMS: Adding migrations to import clinic-providers and provider-cl… (#652)
    * UWM-CMS: Adding migrations to import clinic-providers and provider-clinics from BiosResevoir.
    
    * UWM-CMS: Adding migration for taxonomy keywords to match bios-reservoir term fields.

---
+++ docroot/modules/custom/uwm_res_import/src/Plugin/migrate/process/UwmNidForUUID.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Drupal\uwm_res_import\Plugin\migrate\process;
+
+use Drupal\migrate\MigrateExecutableInterface;
+use Drupal\migrate\ProcessPluginBase;
+use Drupal\migrate\Row;
+
+/**
+ * Perform custom value transformations.
+ *
+ * @MigrateProcessPlugin(
+ *   id = "uwm_nid_for_uuid"
+ * )
+ *
+ * To do custom value transformations use the following:
+ *
+ * @code
+ * field_text:
+ *   plugin: transform_value
+ *   source: text
+ * @endcode
+ */
+class UwmNidForUUID extends ProcessPluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
+
+    if ($value) {
+
+      $value = !empty($value['id']) ? $value['id'] : $value;
+
+      $nodes = \Drupal::entityTypeManager()
+        ->getStorage('node')
+        ->loadByProperties(['uuid' => $value]);
+
+      if (!empty($nodes) && $node = reset($nodes)) {
+        return $node->id();
+      }
+
+      return NULL;
+    }
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function prepareRow(Row $row) {
+    $nid = $row->getSourceProperty('nid');
+  }
+
+}
