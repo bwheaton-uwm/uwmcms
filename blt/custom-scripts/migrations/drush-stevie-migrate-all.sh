6fe1c41a48ba33c6534c0931302ed77988ba49b0
sodacrackers <1248488+sodacrackers@users.noreply.github.com>
2019-02-20 10:18:37 -0800

    Hotfix/clinic proivders import fix (#682)
    * UWM-STEVIE: Adding migrate yml fix to include clinic's providers.
    
    * UWM-STEVIE: Adding import for uwmed to stevie clinic body text.
    
    * UWM-STEVIE: Adding import for uwmed to stevie clinic body text.

---
+++ blt/custom-scripts/migrations/drush-stevie-migrate-all.sh
@@ -0,0 +1,24 @@
+
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_res_tax__affiliates --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_res_tax__board_certifications --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_res_tax__buildings --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_res_tax__campuses --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_res_tax__degrees --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_res_tax__ed --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_res_tax__education_institutions --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_res_tax__education_types --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_res_tax__floors --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_res_tax__languages --update --force 
+
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_res_clinics_import_clinics --update --force 
+
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_providers_import_providers --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_providers_import_providers1 --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_providers_import_providers2 --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_providers_import_providers3 --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_providers_import_providers4 --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_providers_import_providers5 --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_providers_import_providers6 --update --force 
+
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_res_specialitie_tax__expertise --update --force 
+/usr/local/bin/drush9 --uri=stevie.cmsdev.uwmedicine.org migrate-import uwm_res_specialitie_tax__medical_services --update --force 
