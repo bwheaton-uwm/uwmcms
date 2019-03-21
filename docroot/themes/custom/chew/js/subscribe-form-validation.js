6c21744837a5e587d3cf408f2641911c1dd319dd
Brian Tofte-Schumacher <hello@briantschu.com>
2019-03-21 16:53:58 -0700

    UWM-CHEW: Update webform, capture email subscriptions (#746)
    * UWM-CMS: Update webform to 8.x-5.1, add webform_rest
    
    * Ran numerous database updates for default, chew, huddle, stevie then exported config updates to match
    * Uninstall contribute module since upgrading webform removed it via composer
    
    * UWM-CHEW: Enable webform_rest
    
    * UWM-CHEW: Capture email subscriptions in Drupal webform

--- docroot/themes/custom/chew/js/subscribe-form-validation.js
+++ docroot/themes/custom/chew/js/subscribe-form-validation.js
@@ -1,6 +1,6 @@
 /**
  * @file
- * jQuery Validation for email subscribe form.
+ * Imlpement jQuery Validation for email subscribe form.
  */
 
 (function ($, Drupal) {
@@ -31,7 +31,25 @@
             email: "Please enter a valid email address."
           }
         },
-        errorClass: "text-danger"
+        errorClass: "text-danger",
+        submitHandler: function (form) {
+          // Disable the submit button.
+          $(form).find("button").attr("disabled", true);
+
+          // Gather the subscribe form possible values.
+          var firstName = $(form).find("input#firstName").val();
+          var lastName = $(form).find("input#lastName").val();
+          var email = $(form).find("input#emailAddress").val();
+
+          // Find the hidden Drupal webform.
+          var $webform = $(".webform-submission-email-subscriptions-form");
+
+          // Populate Drupal webform values and submit.
+          $webform.find("input[name='first_name']").val(firstName);
+          $webform.find("input[name='last_name']").val(lastName);
+          $webform.find("input[name='email_address']").val(email);
+          $webform.submit();
+        }
       });
     }
   };
