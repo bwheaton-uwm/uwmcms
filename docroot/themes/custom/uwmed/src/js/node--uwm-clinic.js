5aa1f74beef449e9619d78a00fec5ccac09705ea
Tory G <tea-time@users.noreply.github.com>
2019-03-26 14:09:08 -0700

    Merge #751: features/clinic-card-address-link-324716072 > develop
    * UWM-UWMED: in clinic page JS, ensure existing Overview and Directions jump link click events happen only once by using 'context' arg
    
    * UWM-UWMED: clinic pages - if #directions-tab is in url, scroll to Directions tab; update other jump links to the same animated scroll
    
    * UWM-CMS: update uwmbase to latest commit on features-search - corresponding merge commit for features/clinic-card-address-link-324716072 branch
    
    * UWM-CMS: update uwmbase to latest merge commit of features/clinic-card-address-link-324716072 into features-search
    
    * UWM-CMS: update uwmbase to dev-develop merge commit for features/clinic-card-address-link-324716072

--- docroot/themes/custom/uwmed/src/js/node--uwm-clinic.js
+++ docroot/themes/custom/uwmed/src/js/node--uwm-clinic.js
@@ -1,29 +1,86 @@
 /**
+ * @file
  * Scripting for clinic-type nodes.
  */
 
-
 (function ($, Drupal, drupalSettings) {
 
-    'use strict';
+  'use strict';
 
-    Drupal.behaviors.clinicExtraTabLinks = {
-        attach: function (context, settings) {
+  Drupal.behaviors.clinicExtraTabLinks = {
+    attach: function (context, settings) {
 
-          $('section.clinic-header a[href="#directions-jump"]').click(function () {
-            var href = '#directions-tab';
-            $('.nav-tabs a[href="' + href + '"]').trigger('click');
-          });
+      var $clinicTabs = $('.clinic-tabs .nav-tabs a[data-toggle="tab"]');
+      var $mainTab = $('.clinic-tabs .nav-tabs a[href="#main-tab"]');
+      var $directionsTab = $('.clinic-tabs .nav-tabs a[href="#directions-tab"]');
 
-          // trigger main tab when user clicks on clinic overview, then jump
-          $('section.clinic-header a[href="#clinic-overview-jump"]').click(function (e) {
-            e.preventDefault();
-            var href = "#main-tab";
-            $('.nav-tabs a[href="' + href + '"]').trigger('click');
-            window.location.href = '#clinic-overview-jump';
-          });
+      // Helper that triggers showing a tab (if not already active), and setting
+      // an element within the tab to which to scroll.
+      var showTabAndScroll = function ($tab, $scrollTo, setHash) {
+        $tab.data('scrollTo', $scrollTo);
 
+        if (!$tab.parent('li').hasClass('active')) {
+          $tab.trigger('click');
         }
-    };
+        else {
+          // If a tab is already active, triggering the click on it does not
+          // result in the 'shown.bs.tab' event. But we want to run our handler
+          // (for scrolling) nonetheless.
+          $tab.triggerHandler('shown.bs.tab');
+        }
+
+        // Set a hash if requested, to add a history entry.
+        if (setHash !== undefined) {
+          window.location.hash = setHash;
+        }
+      };
+
+      // Tab-shown handler: scroll to element within tab, if specified via
+      // 'scrollTo' data attr.
+      // @see https://getbootstrap.com/docs/3.3/javascript/#tabs-methods
+      $clinicTabs.on('shown.bs.tab', function () {
+        var $tab = $(this);
+        var $scrollTo = $tab.data('scrollTo');
+
+        if ($scrollTo && $scrollTo.length > 0) {
+          $('html, body').animate({
+            'scrollTop': $scrollTo.offset().top
+          }, 700);
+
+          // Remove the scrollTo element, so it's only used when our custom code
+          // sets it before a tab is shown.
+          $tab.data('scrollTo', null);
+        }
+      });
+
+      // On page load with the Directions tab hash, scroll to top of tabs.
+      // (Bootstrap tab history shows the tab based on hash, but does not scroll
+      // to it.)
+      if (context === document && window.location.hash === '#directions-tab') {
+        // The tabs are in a container positioned with a negative top margin.
+        // There seems to be a race condition - just after page load, the top
+        // offset of the tabs is computed as if without the margin-top.
+        // Give an extra bit of time to hopefully compute the final position.
+        // (If this is not enough, the position is at the tab content, which
+        // is a reasonable fallback.)
+        window.setTimeout(function () {
+          showTabAndScroll($directionsTab, $directionsTab);
+        }, 150);
+      }
+
+      // On Directions jump link click, scroll to top of tabs.
+      $('section.clinic-header a[href="#directions-jump"]', context).click(function (e) {
+        e.preventDefault();
+        showTabAndScroll($directionsTab, $directionsTab);
+      });
+
+      // On Clinic Overview jump link click, scroll to overview section within
+      // main Services/Resources tab.
+      $('section.clinic-header a[href="#clinic-overview-jump"]', context).click(function (e) {
+        e.preventDefault();
+        showTabAndScroll($mainTab, $('#clinic-overview-jump'), '#clinic-overview-jump');
+      });
+    }
+  };
 
 })(jQuery, Drupal, drupalSettings);
