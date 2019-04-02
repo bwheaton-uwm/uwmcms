f585cd389b33bef375d0b780b7062caea6c0955b
Tory G <tea-time@users.noreply.github.com>
2019-04-02 10:42:30 -0700

    features/adjust-ga-location-cards-334808398-2 > develop (#786)
    * UWM-CMS: on location search event tracking, add precaution in selecting elements on the page for logged-in users
    
    * UWM-CMS: Update tracking to match spec

--- docroot/modules/custom/uwmed_uwmcms_gtm/js/location-search-page.js
+++ docroot/modules/custom/uwmed_uwmcms_gtm/js/location-search-page.js
@@ -12,27 +12,42 @@
         return;
       }
 
-      var searchForm = $('#views-exposed-form-uwm-locations-geo-search-page-1', context);
+      var searchFormSelector = '#views-exposed-form-uwm-locations-geo-search-page-1';
+      var searchFormInContext = $(searchFormSelector, context);
+      var searchFormInPage = $(searchFormSelector);
 
       // Add event tracking to the location search submit link.
-      searchForm.siblings('.submit-wrapper').find('a.btn-cta.submit').one('click', function (event) {
-        var searchLocationTerm = searchForm.find('input[name=l]').val();
-        searchLocationTerm = searchLocationTerm === "" ? "null" : searchLocationTerm.toLowerCase();
+      // In this case, form should be within context, since we are attaching to
+      // one of its elements.
+      if (searchFormInContext.length > 0) {
+        searchFormInContext.siblings('.submit-wrapper').find('a.btn-cta.submit').one('click', function (event) {
+          var searchTerm = searchFormInContext.find('input[name=s]').val();
+          searchTerm = searchTerm === "" ? "null" : searchTerm;
+          var searchLocationTerm = searchFormInContext.find('input[name=l]').val();
+          searchLocationTerm = searchLocationTerm === "" ? "null" : searchLocationTerm.toLowerCase();
 
-        dataLayer.push({
-          'event': 'search initiation',
-          'searchType': 'location',
-          'searchLocation': 'location search page',
-          'searchTerm': searchForm.find('input[name=s]').val(),
-          'locationTerm': searchLocationTerm
+          dataLayer.push({
+            'event': 'search initiation',
+            'searchType': 'location',
+            'searchLocation': 'location search page',
+            'searchTerm': searchTerm,
+            'locationTerm': searchLocationTerm
+          });
         });
-      });
+      }
 
       // Get values for search result tracking. Only need to get these once when
       // the page loads since the page reloads when a new search is performed.
-      var searchTerm = searchForm.find('input[name=s]').val();
-      var searchLocationTerm = searchForm.find('input[name=l]').val();
-      searchLocationTerm = searchLocationTerm === "" ? "null" : searchLocationTerm.toLowerCase();
+      // In this case, we'll be attaching to card elements in context (below),
+      // but the form just needs to be on the page.
+      var searchTerm = "null";
+      var searchLocationTerm = "null";
+      if (searchFormInPage.length > 0) {
+        searchTerm = searchFormInPage.find('input[name=s]').val();
+        searchTerm = searchTerm === "" ? "null" : searchTerm;
+        searchLocationTerm = searchFormInPage.find('input[name=l]').val();
+        searchLocationTerm = searchLocationTerm === "" ? "null" : searchLocationTerm.toLowerCase();
+      }
 
       // Add phone number click tracking to location card phone numbers.
       $('.view-uwm-locations-geo-search .clinic-card a[href^="tel:"]', context).each(function (index) {
@@ -42,7 +57,8 @@
             'event': 'phone number click',
             'searchType': 'location',
             'searchTerm': searchTerm,
-            'linkUrl': clinicLink
+            'linkUrl': clinicLink,
+            'locationTerm': searchLocationTerm
           });
         });
       });
@@ -52,27 +68,12 @@
       // all should have rel="bookmark" to easily track them here.
       $('.view-uwm-locations-geo-search .clinic-card a[rel="bookmark"]', context).each(function (index) {
         $(this).on('click', function (event) {
-          var uxElement = '';
-
-          if ($(this).parents(".clinic-card__image").length > 0) {
-            uxElement = 'image';
-          }
-          else if ($(this).parent().hasClass('clinic-card__name')) {
-            uxElement = 'title';
-          }
-          else if ($(this).parent().hasClass('see-more')) {
-            uxElement = 'services';
-          }
-          else if ($(this).parent().hasClass('clinic-card__cta')) {
-            uxElement = 'linktodetail';
-          }
-
           dataLayer.push({
             'event': 'search result click',
             'searchType': 'location',
             'searchTerm': searchTerm,
             'linkURL': $(this).attr('href'),
-            'uxElement': uxElement,
+            'uxElement': 'linktodetail',
             'locationTerm': searchLocationTerm
           });
         });
@@ -85,7 +86,7 @@
             'searchType': 'location',
             'searchTerm': searchTerm,
             'linkURL': $(this).attr('href').replace('#directions-tab', ''),
-            'uxElement': 'address',
+            'uxElement': 'linktodetail',
             'locationTerm': searchLocationTerm
           });
         })
