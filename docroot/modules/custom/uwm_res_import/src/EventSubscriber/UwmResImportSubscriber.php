d6518042b235fa63368bfd674bc8c7466cf72e3e
Nick <meyerjn@uw.edu>
2019-04-10 19:54:47 -0700

    UWM-STEVIE: Updating provider and provider image imports. Imports provider image files from reservoir and adds them to provider nodes.

--- docroot/modules/custom/uwm_res_import/src/EventSubscriber/UwmResImportSubscriber.php
+++ docroot/modules/custom/uwm_res_import/src/EventSubscriber/UwmResImportSubscriber.php
@@ -56,17 +56,28 @@ class UwmResImportSubscriber implements EventSubscriberInterface {
    */
   public function preRowSave(MigratePreRowSaveEvent $event) {
 
-    /** @var \Drupal\migrate\Row $row */
-    // $row = $event->getRow();
-    /** @var \Drupal\migrate\Plugin\MigrationInterface $migration */
-    // $migration = $event->getMigration();
-    // $id_map = $event->getRow()->getIdMap();
-    // if (!empty($id_map['destid1'])) {
-    // $this->preExistingItem = TRUE;
-    // }
-    // else {
-    // $this->preExistingItem = FALSE;
-    // }.
+    $migration = $event->getMigration();
+    $source = $row->getSource();
+
+    // @TODO: Move this to provider import yml.
+    // The migration_lookup plugin was
+    // not returning destination file id's so,
+    // populate the image field here.
+    if (!empty($source['field_res_image']['data'][0]['id'])) {
+
+      $source_uuid = $source['field_res_image']['data'][0]['id'];
+      $fid = self::getMigrationDestinationId($source_uuid, $migration->id());
+
+      $image = [
+        'target_id' => $fid,
+        'alt' => 'Alt text',
+        'title' => 'Title',
+      ];
+
+      $row->setDestinationProperty('field_res_image', $image);
+
+    }
+
   }
 
   /**
@@ -95,6 +106,35 @@ class UwmResImportSubscriber implements EventSubscriberInterface {
 
     return TRUE;
 
+  }
+
+  /**
+   * Lookup the local id for item imported earlier, based on it's unique id.
+   *
+   * @param \Drupal\uwm_res_import\EventSubscriber\string $sourceId
+   *   Description here.
+   * @param \Drupal\uwm_res_import\EventSubscriber\string $migrationName
+   *   Description here.
+   *
+   * @return null
+   *   Description here.
+   */
+  private static function getMigrationDestinationId(string $sourceId, string $migrationName) {
+
+    $table = 'migrate_map_' . $migrationName;
+    $table = 'migrate_map_uwm_res_providers_import_provider_images';
+
+    $connection = \Drupal::database();
+    $query = $connection->query("SELECT * FROM {$table} WHERE sourceid1 = :id", [
+      ':id' => $sourceId,
+    ]);
+    $result = $query->fetchAll();
+
+    if ($result && !empty($result[0]->destid1)) {
+      return $result[0]->destid1;
+    }
+
+    return NULL;
   }
 
 }
