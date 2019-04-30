f3a9ea2c6277ccb5658c3538c312e7cff6f8eeb3
Brian Tofte-Schumacher <btschu@uw.edu>
2019-04-30 10:35:38 -0700

    features/provider-card-appointments > master (#846)
    * UWM-CMS: Create Epic ID and open scheduling boolean fields
    
    * UWM-CMS: Add 'book online' button
    
    * UWM-CMS: Add event tracking for book online button
    
    * UWM-CMS: Add event tracking for book online button
    
    * UWM-CMS: Add contact information to provider card
    
    * UWM-CMS: Update text on 'view contact details' button
    
    * UWM-CMS: Clarify comments and make sure entity reference item exists
    
    * UWM-CMS: Move provider card styles out of base
    
    * UWM-CMS: Add urgent care provider logic
    
    * UWM-CMS: Use full www.uwmedicine.org URL for provider link
    
    * UWM-CMS: Shrink padding on accepting new patients
    
    * UWM-CMS: Add event tracking to view contact details button and clarify comments
    
    * UWM-CMS: Update margin on angle-right
    
    * UWM-CMS: Hide locations for urgent care providers

--- docroot/modules/custom/uwmed_uwmcms_gtm/js/provider-search-page.js
+++ docroot/modules/custom/uwmed_uwmcms_gtm/js/provider-search-page.js
@@ -43,34 +43,43 @@
         }
       });
 
-      // Add event tracking to the provider search results links (image, title, cta).
+      // Initialize search result click event.
+      var searchResultClick = {
+        'event': 'search result click',
+        'searchType': 'provider',
+        'searchTerm': searchTerm
+      };
+
+      // Add event tracking to the provider search result card linktodetail links to the provider page (image, title, cta).
       $('.view-uwm-providers-search', context).find('.view-mode-provider_card a[rel="bookmark"], .view-mode-provider_card .provider-card__image a').each(function (index) {
         $(this).one('click', function (event) {
-          dataLayer.push({
-            'event': 'search result click',
-            'searchType': 'provider',
-            'searchTerm': searchTerm,
-            'linkURL': $(this).attr('href'),
-            'uxElement': 'linktodetail'
-          });
+          searchResultClick.linkURL = $(this).attr('href');
+          searchResultClick.uxElement = 'linktodetail';
+          dataLayer.push(searchResultClick);
         });
       });
 
-      // Add event tracking to the provider search locations links.
-      $('.view-uwm-providers-search .provider-card__locations a', context).each(function (index) {
+      // Add event tracking to the provider search result card linktodetail links to other pages (locations and view contact details).
+      $('.view-uwm-providers-search', context).find('.provider-card__locations a, .provider-card__contact-information').each(function (index) {
         $(this).one('click', function (event) {
-          var providerLink = $(this).parents('.view-mode-provider_card').find('a[rel="bookmark"]').first().attr('href');
-          dataLayer.push({
-            'event': 'search result click',
-            'searchType': 'provider',
-            'searchTerm': searchTerm,
-            'linkURL': providerLink,
-            'uxElement': 'linktodetail'
-          });
+          // Set card's provider bio link as the linkURL for this click event.
+          searchResultClick.linkURL = $(this).parents('.view-mode-provider_card').find('a[rel="bookmark"]').first().attr('href');
+          searchResultClick.uxElement = 'linktodetail';
+          dataLayer.push(searchResultClick);
         });
       });
 
-      // Add phone number click tracking to provider card phone numbers.
+      // Add click event tracking to provider search result card bookonline links (book now button).
+      $('.view-uwm-providers-search .provider-card__book-online', context).each(function (index) {
+        $(this).one('click', function (event) {
+          // Set card's provider bio link as the linkURL for this click event.
+          searchResultClick.linkURL = $(this).parents('.view-mode-provider_card').find('a[rel="bookmark"]').first().attr('href');
+          searchResultClick.uxElement = 'bookonline';
+          dataLayer.push(searchResultClick);
+        });
+      });
+
+      // Add phone number event tracking to provider search result card phone numbers.
       $('.view-uwm-providers-search .view-mode-provider_card a[href^="tel:"]', context).each(function (index) {
         $(this).one('click', function (event) {
           var providerLink = $(this).parents('.view-mode-provider_card').find('a[rel="bookmark"]').first().attr('href');
