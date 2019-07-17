adf1f5d6b4af09922c6e44615137f15039b95d5b
sodacrackers <meyerjn@uw.edu>
2019-07-17 10:56:28 -0700

    UWM-RESERVOIR: Adding fields to template to show featured locations on medical service pages.

---
+++ docroot/themes/custom/uwmbase/components/medical-service/medical-services.js
@@ -0,0 +1,24 @@
+/**
+ * @file
+ * Provides features specific to our medical-service pages.
+ */
+
+(function (Drupal, $) {
+
+    'use strict';
+
+    /**
+     * Main function for initing custom Medical Service routines.
+     *
+     * @type {Drupal~behavior}
+     *
+     * @prop {Drupal~behaviorAttach} attach
+     *   Specific description of this attach function goes here.
+     */
+    Drupal.behaviors.medicalServicesMain = {
+        attach: function () {
+
+        }
+    };
+
+})(Drupal, jQuery);
