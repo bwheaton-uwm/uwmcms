a048c4552e6e4e90bbe8adea1b52945dc7a8c8b6
sodacrackers <meyerjn@uw.edu>
2019-08-22 22:03:59 -0700

    UWM-CMS: Triggering updating in stevie settings to put auth cookie protection on dev and stage.

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -802,6 +802,22 @@ if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
 }
 
 
+if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
+
+  if ($_ENV['AH_SITE_ENVIRONMENT'] !== 'prod') {
+
+    /***
+     * Require a simple auth password on pre-production:
+     */
+    $file = DRUPAL_ROOT . '/sites/uwm-require-auth-cookie-page-blocker.php';
+    if (is_file($file)) {
+        include_once $file;
+    }
+
+  }
+
+}
+
 /**
  * Override domain detection in Acquia Purge.
  */
@@ -843,22 +859,6 @@ else {
 
 }
 
-
-if (isset($_ENV['AH_SITE_ENVIRONMENT'])) {
-
-  if ($_ENV['AH_SITE_ENVIRONMENT'] !== 'prod') {
-
-    /***
-     * Require a simple password on pre-production:
-     */
-    $file = DRUPAL_ROOT . '/sites/uwm-require-auth-cookie-page-blocker.php';
-    if (is_file($file)) {
-      include_once $file;
-    }
-
-  }
-
-}
 
 
 /**
