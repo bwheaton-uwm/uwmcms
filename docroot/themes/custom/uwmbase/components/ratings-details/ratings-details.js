a6f1aec73c29e5e53ddd9cb32b6d9ebd9af94edc
Rick Hawkins <rick.hawkins@slalom.com>
2019-11-05 07:44:20 -0800

    UWM-STEVIE: Customize ratings details pagination

--- docroot/themes/custom/uwmbase/components/ratings-details/ratings-details.js
+++ docroot/themes/custom/uwmbase/components/ratings-details/ratings-details.js
@@ -63,15 +63,23 @@
         // Add extra classs to the page links, scroll on click, remove the
         // anchor tag from the any ellipses, and add span wrappers and icons.
         afterRender: function () {
-          $('.pager__items li').addClass('pager__item').click(function () {
-            scrollToShowingInfo();
+          $('.pager__items li').addClass('pager__item')
+            .not('.paginationjs-ellipsis').click(function () {
+              scrollToShowingInfo();
           });
           $('.paginationjs-prev').addClass('pager__item--text pager__item--previous').find('a').contents().before('<i class="fa fa-angle-left arrow-linked" aria-hidden="true"></i>').wrap('<span class="link-text"></span>');
           $('.paginationjs-next').addClass('pager__item--text pager__item--next').find('a').contents().after('<i class="fa fa-angle-right arrow-linked" aria-hidden="true"></i>').wrap('<span class="link-text"></span>');
           $('.paginationjs-ellipsis').addClass('pager__item--text pager__item--ellipsis').find('a').contents().unwrap();
 
+          $('.paginationjs-page')
+            .first().addClass('pager__item-first').end()
+            .last().addClass('pager__item-last');
+
+          var activePage = $('.paginationjs-page.is-active');
+          var lastPageNum = $('.pager__item-last').attr('data-num');
+
           // Add offset classes.
-          $('.paginationjs-page.is-active')
+          activePage
             .addClass('current-offset-0')
             .prevAll('.paginationjs-page:not(:eq(-1))')
             .addClass(function (index) {
@@ -83,6 +91,27 @@
               return 'current-offset-' + (index + 1);
             }
           );
+
+          switch (activePage.attr('data-num')) {
+            // If the active page is fourth from either end, prevent hiding
+            // of the second page from the end at narrow widths.
+            case "4":
+              $('.pager__items').find('[data-num="2"]').addClass('d-block');
+              break;
+
+            case (lastPageNum - 3).toString():
+              $('.pager__items').find('[data-num="' + (lastPageNum - 1) + '"]').addClass('d-block');
+              break;
+
+            // If the active page is fifth from either end, add an ellipsis.
+            case "5":
+              $('.paginationjs-page').first().after('<li class="paginationjs-ellipsis disabled pager__item pager__item--text pager__item--ellipsis d-sm-none">...</li>');
+              break;
+
+            case (lastPageNum - 4).toString():
+              $('.paginationjs-page').last().before('<li class="paginationjs-ellipsis disabled pager__item pager__item--text pager__item--ellipsis d-sm-none">...</li>');
+              break;
+          }
         }
       });
     }
