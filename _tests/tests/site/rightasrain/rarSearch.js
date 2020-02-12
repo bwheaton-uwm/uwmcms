4c2212930c7cef6bc02d6e7e7293e01eaab608cb
sodacrackers <meyerjn@uw.edu>
2020-02-12 13:35:18 -0800

    UWM-TESTS: Adding Right as Rain search test.

---
+++ _tests/tests/site/rightasrain/rarSearch.js
@@ -0,0 +1,79 @@
+module.exports = {
+
+  "disabled": false,
+  "@tags": [
+    "rar",
+    "search",
+  ],
+
+  before: function (browser) {
+  },
+
+  after: function (browser) {
+  },
+
+  beforeEach: function (browser) {
+  },
+
+  afterEach: function (browser) {
+  },
+
+
+  'Verifying we can search for "heart" results': function (browser) {
+
+    browser
+      .url('https://rightasrain.uwmedicine.org/')
+
+    browser
+      .assert.visible('.region-header input[name=search]')
+      .setValue('.region-header input[name=search]', 'Sleep')
+      .assert.visible('.region-header button[type=submit]');
+
+    browser.click('.region-header button[type=submit]', function (result) {
+      console.log('Click result', result);
+    });
+
+
+  },
+
+  'Verifying a search result is clickable': function (browser) {
+
+    let linkSelector = '.view-content .views-row:last-of-type a';
+    let articleUrl;
+    browser
+      .waitForElementVisible('.region-content .view-content  .views-row')
+      .assert.containsText('.region-content .view-content', 'Sleep');
+
+    browser.assert.visible(linkSelector);
+
+    browser.getAttribute('css selector', linkSelector, 'href', function(result) {
+
+      articleUrl = result.value;
+      console.log('linkSelector href', result);
+      console.log('articleUrl', articleUrl);
+
+      browser.url(articleUrl);
+
+    });
+
+
+
+
+  },
+
+  'Verifying we have article content and an author': function (browser) {
+
+
+    browser.waitForElementVisible('.main-container')
+      .getText('.field--name-field-author', function (result) {
+        browser.assert.ok(result.value.length > 10);
+      })
+      .getText('.field--name-field-sections .field--name-field-body', function (result) {
+        browser.assert.ok(result.value.length > 400);
+      });
+
+
+  }
+
+
+};
--- _tests/tests/site/rightasrain/rightAsRain.js
+++ _tests/tests/site/rightasrain/rightAsRain.js
@@ -11,32 +11,17 @@ module.exports = {
   url: "https://rightasrain.uwmedicine.org/",
 
   before: function (browser) {
-
-    console.log('Setting up...');
-
   },
 
   after: function (browser) {
-
-    console.log('Closing down...');
-    browser.end();
-
   },
 
   beforeEach: function (browser) {
-
-    console.log('beforeEach up...');
-
-    browser.url(function (result) {
-      console.log(result);
-    });
-
   },
 
   afterEach: function (browser) {
 
     console.log('afterEach up...');
-
     browser.url(function (result) {
       console.log(result);
     });
@@ -47,14 +32,23 @@ module.exports = {
 
     browser
       .url('https://rightasrain.uwmedicine.org/')
-      .assert.titleContains('Rain')
-      .waitForElementVisible('.region-content')
-      .assert.containsText('.region-content', 'Latest');
-
+      .assert.titleContains('Rain');
 
   },
 
-  'Verifying we have a featured article, and it\'s image and summary': function (browser) {
+
+  'Verifying we have at least four "latest" section articles': function (browser) {
+
+    browser
+      .waitForElementVisible('.region-content')
+      .assert.containsText('.region-content', 'Latest');
+
+    browser.expect.element('.view-latest-articles *:nth-child(4) h3 a').to.be.present;
+
+  },
+
+
+  'Verifying we have a featured article, it\'s image and summary': function (browser) {
 
     browser
       .assert.containsText('.region-content ' +
@@ -65,60 +59,6 @@ module.exports = {
       '.block-views-blockhighlighted-article-block ' +
       '.field--name-field-primary-media img');
 
-
   },
-
-  'Verifying we can search for "heart" results': function (browser) {
-    browser
-      .assert.visible('.region-header input[name=search]')
-      .setValue('.region-header input[name=search]', 'Sleep')
-      .assert.visible('.region-header button[type=submit]');
-
-    browser.click('.region-header button[type=submit]', function (result) {
-      console.log('Click result', result);
-    });
-
-
-  },
-
-  'Verifying a search result is clickable': function (browser) {
-
-    let linkSelector = '.view-content .views-row:last-of-type a';
-    let articleUrl;
-    browser
-      .waitForElementVisible('.region-content .view-content  .views-row')
-      .assert.containsText('.region-content .view-content', 'Sleep');
-
-    browser.assert.visible(linkSelector);
-
-    browser.getAttribute('css selector', linkSelector, 'href', function(result) {
-
-      articleUrl = result.value;
-      console.log('linkSelector href', result);
-      console.log('articleUrl', articleUrl);
-
-      browser.url(articleUrl);
-
-    });
-
-
-
-
-  },
-
-  'Verifying we have article content and an author': function (browser) {
-
-
-    browser.waitForElementVisible('.main-container')
-      .getText('.field--name-field-author', function (result) {
-        browser.assert.ok(result.value.length > 10);
-      })
-      .getText('.field--name-field-sections .field--name-field-body', function (result) {
-        browser.assert.ok(result.value.length > 400);
-      });
-
-
-  }
-
 
 };
