820fc6e081bcf1d2f2ded3ff20e2229aeba8f099
Gray Embry <jasongem@uw.edu>
2020-02-12 15:14:53 -0800

    Fix for rarSearch.js

--- _tests/tests/site/rightasrain/rarSearch.js
+++ _tests/tests/site/rightasrain/rarSearch.js
@@ -3,7 +3,7 @@ module.exports = {
   "disabled": false,
   "@tags": [
     "rar",
-    "search",
+    "search"
   ],
 
   before: function (browser) {
@@ -18,9 +18,7 @@ module.exports = {
   afterEach: function (browser) {
   },
 
-
   'Verifying we can search for "heart" results': function (browser) {
-
     browser
       .url('https://rightasrain.uwmedicine.org/')
 
@@ -32,38 +30,22 @@ module.exports = {
     browser.click('.region-header button[type=submit]', function (result) {
       console.log('Click result', result);
     });
-
-
   },
 
   'Verifying a search result is clickable': function (browser) {
-
     let linkSelector = '.view-content .views-row:last-of-type a';
-    let articleUrl;
+    debugger;
     browser
       .waitForElementVisible('.region-content .view-content  .views-row')
       .assert.containsText('.region-content .view-content', 'Sleep');
 
-    browser.assert.visible(linkSelector);
-
-    browser.getAttribute('css selector', linkSelector, 'href', function(result) {
-
-      articleUrl = result.value;
-      console.log('linkSelector href', result);
-      console.log('articleUrl', articleUrl);
-
-      browser.url(articleUrl);
-
-    });
-
-
-
-
+    browser
+      .waitForElementVisible(linkSelector)
+      .click("css selector", linkSelector)
+      .assert.urlContains("sleep-paralysis");
   },
 
   'Verifying we have article content and an author': function (browser) {
-
-
     browser.waitForElementVisible('.main-container')
       .getText('.field--name-field-author', function (result) {
         browser.assert.ok(result.value.length > 10);
@@ -71,9 +53,5 @@ module.exports = {
       .getText('.field--name-field-sections .field--name-field-body', function (result) {
         browser.assert.ok(result.value.length > 400);
       });
-
-
   }
-
-
 };
