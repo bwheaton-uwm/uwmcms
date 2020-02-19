5c0d25e64ae1f41f7637e55863e3d8bfdb9e0c48
sodacrackers <meyerjn@uw.edu>
2020-02-19 11:57:18 -0800

    UWM-CMS: Testing travis build options.

---
+++ patches/phpunit_exit_nice.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -e
+
+#
+# Shell script to have phpunit exit quietly if called with no options.
+#
+# BLT is running this command during execution on TRavis-CI:
+# `$ $TRAVIS_BUILD_DIR/vendor/bin/phpunit`
+# Because phpunit is called without any options, it shows help text and
+# exits with exit code 2. That breaks the build.
+#
+# This script/ patch can be removed once linting passes and BLT knows phpunit
+# is called upstream during other build test steps.
+#
+# Until then, make phpunit exit 0 if called without args.
+#
+#
+
+echo ""
+echo ""
+echo "UWM-CMS: Making phpunit exit 0, if called with no options."
+
+FILE=vendor/bin/phpunit
+if [ ! -d $SIMPSAML ]; then
+    FILE=../../vendor/bin/phpunit
+
+fi
+
+MATCH='require PHPUNIT_COMPOSER_INSTALL;'
+REPLACE='/*** UWMCMS Patch ***/\
+if(empty($argv) || count($argv) < 2) exit (0);\
+\
+'
+
+sed -i '' "s/${MATCH}/${REPLACE}${MATCH}/g" $FILE
+
+
+echo ""
+echo ""
+
+
+
+
+
+set +v
